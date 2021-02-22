035d1938a85cf77ff27cc0ec21c327afcea49b4e
5ayam5 <sayam.sethi2@gmail.com>
2021-02-22 16:59:37 +0530

    Revert "fixed issues, need to implement positive area formula"
    This reverts commit 2c2013a11d9133e23caa7e35a4c4826f4b5d2eb1.

--- area_sayam.asm
+++ area_sayam.asm
@@ -8,37 +8,36 @@
 
 # setting and loading initial values along with handling base case
 main:
-	l.s		$f11, max		# load the max permitted value of x, y
-	l.s		$f10, min		# load the min permitted value of x, y
+	lw		$s7, max		# load the max permitted value of x, y
+	lw		$s6, min		# load the min permitted value of x, y
 
 	li		$v0, 5			# load_int
 	syscall
 
 	move	$t0, $v0		# n: number of coordinates
+    beq		$t0, 1, nIs1	# handle corner case
 	blt		$t0, 1, nLess1	# handle corner case (n<1)
 
 	li		$t1, 1			# counter of points
 	jal		input
-	mov.s	$f1, $f0		# coordinate of x1
+	move	$t2, $v0		# coordinate of x1
 	jal		input
 	mov.s	$f2, $f0		# coordinate of y1
-
-	beq		$t0, 1, nIs1	# handle corner case
 
 	l.s		$f6, zero		# the area
 	l.s		$f5, point5		# load 0.5
 
 
-# taking n coordinates as input and calculating area using a loop
+# taking n coordinates as input and calculating are using a loop
 loop:
 	jal		checkBounds		# check the bounds
 	addi	$t1, $t1, 1		# increment the counter
 	jal		input
-	mov.s	$f3, $f0		# coordinate of x2
+	move	$t4, $v0		# coordinate of x2
 	jal		input
-	mov.s	$f4, $f0		# coordinate of y2
+	move	$t5, $v0		# coordinate of y2
 
-	jal		area			# compute area
+    jal area                # compute area
 	add.s	$f6, $f6, $f1	# increment area
 
 	beq		$t1, $t0, print	# end loop
@@ -50,17 +49,17 @@ loop:
 
 # area function
 area:
-	sub.s	$f1, $f3, $f1	# compute x2-x1
+    sub.s	$f1, $f3, $f1	# compute x2-x1
 	add.s	$f2, $f4, $f2	# compute y1+y2
 	mul.s	$f2, $f2, $f5	# compute (y1+y2)/2
 	mul.s	$f1, $f1, $f2	# compute area of trapezium
 
-	jr		$ra
+    jr $ra
 
 
 # function to take a float input
 input:
-	li		$v0, 6			# read_float
+	li		$v0, 5			# read_int
 	syscall					# takes input and stores in v0
 	jr		$ra				# returns function and jumps to address stored in $ra
 
@@ -73,15 +72,11 @@ output:
 # function to check if the input value is in range
 checkBounds:
 	# x coordinate out of bounds
-	c.lt.s	$f11, $f1
-	bc1t	outOfBounds
-	c.lt.s	$f1, $f10
-	bc1t	outOfBounds
+	bgt		$t2, $s7, outOfBounds
+	blt		$t2, $s6, outOfBounds
 	# y coordinate out of bounds
-	c.lt.s	$f11, $f2
-	bc1t	outOfBounds
-	c.lt.s	$f2, $f10
-	bc1t	outOfBounds
+	bgt		$t3, $s7, outOfBounds
+	blt		$t3, $s6, outOfBounds
 	# all okay, go back
 	jr		$ra
 
@@ -89,28 +84,35 @@ checkBounds:
 outOfBounds:
 	la		$a0, ooB		# load ooB message
 	jal		output
-	j		exit
+	jal		exit
 
 # only one point has area zero
 nIs1:
 	li		$v0, 2			# print 0 as $f12 is initialised to 0
 	l.s		$f12, zero		# load 0 just to be sure
 	syscall
-	j		exit
+	jal		exit
 
 # n is less than 1
 nLess1:
 	la		$a0, errN		# load error message
 	jal		output
-	j		exit
+	jal		exit
 
 # function to print float value
 print:
-	mov.s	$f12, $f6		# the value
+	div		$s0, $s5		# divide by 2 to get area
+	mfhi	$t1				# the remainder
+	mtc1	$t1, $f11
+	cvt.s.w	$f11, $f11
+	mul.s	$f11, $f11, $f5
+	mflo	$t2				# quotient
+	mtc1	$t2, $f12
+	cvt.s.w	$f12, $f12
+	add.s	$f12, $f12, $f11
 	li		$v0, 2			# print_float
 	syscall
 	la		$a0, lf
-	jal		output
 	j		exit
 
 # function to exit
@@ -134,11 +136,11 @@ lf:
 
 # upper limit
 max:
-	.float	1023.0		# @TODO: find the actual upper bound
+	.word	1023		# @TODO: find the actual upper bound
 
 # lower limit
 min:
-	.float	-1024.0		# @TODO: find the actual lower bound
+	.word	-1024		# @TODO: find the actual lower bound
 
 # zero float
 zero:
